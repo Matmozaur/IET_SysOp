94a4c108a6d4ccfff31a4e7c0ce89f6d218c25db
matmozaur <matmozaur@gmail.com>
2020-11-01 12:12:15 +0100

    add comments

--- raising_list.c
+++ raising_list.c
@@ -1,18 +1,22 @@
 #include <stdio.h> 
 #include <stdlib.h> 
   
+// definition of the list node
 struct Node { 
     double data; 
     struct Node* next; 
 };  
   
 int main(int argc, char *argv[]) { 
+    //size of our node plus size of pointer to that data 
     int node_size = sizeof(struct Node)+8;
+    //system allocates 32-bytes blocks of memory so we have to take it into consider
     node_size += 32%node_size;
     struct Node* head = NULL; 
     head = (struct Node*)malloc(sizeof(struct Node)); 
     head->data = 1.0;
-
+    //subtract memory allocate to program without any list
+    //(checked in htop can be different in various systems)
     int memory = atoi(argv[1]) - 4504000;
     struct Node* previous = head;
     for(int i=0;i<memory/node_size;i++) {
@@ -22,8 +26,6 @@ int main(int argc, char *argv[]) {
         previous = next;
     }
     printf("Press any key to continue.\n");
-    // char c;
-    // scanf("%c\n", &c); 
     getchar();
     return 0; 
 }
